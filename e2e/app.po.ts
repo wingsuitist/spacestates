b23d42843a2c20fc7911df8d3b5b5ac1367bb253
Jonas Felix <jonas.felix@felixideas.ch>
2016-10-02 18:14:19 +0200

    update current angular-cli version

--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,9 +1,11 @@
+import { browser, element, by } from 'protractor';
+
 export class SpacestatesPage {
   navigateTo() {
     return browser.get('/');
   }
 
   getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+    return element(by.css('sps-root h1')).getText();
   }
 }
